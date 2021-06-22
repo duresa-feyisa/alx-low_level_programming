74529687a6524b84d18fb476d2a89e08d5f99f9a
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-06-22 07:59:09 -0700

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: - integer value
+ * @b: - integer value
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
