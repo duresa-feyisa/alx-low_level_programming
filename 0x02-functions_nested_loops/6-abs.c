41245fc22c23e5b753c27274483d60ea5f169045
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-06-22 07:14:03 -0700

    computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _abs - Entry point
+ *
+ * @n: - integer number
+ *
+ * Return: result>0
+ */
+
+int _abs(int n)
+{
+	int result;
+
+	result = abs(n);
+	return (result);
+}
