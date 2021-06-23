85c070413741659a63bec3ca7675421fd063a42d
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-06-23 07:28:07 -0700

    prints the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
-
 /**
  * main - check the code for ALX School students.
  * @n - integer value
  * Return: void.
  */
-
 void print_times_table(int n)
 {
-	int x, y, z, u, d, a, b;
+	int x, y, z;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n < 15)
 	{
 		for (x = 0; x <= n; x++)
 		{
@@ -19,29 +17,19 @@ void print_times_table(int n)
 				z = x * y;
 				if (z > 99)
 				{
-					u = z % 10;
-					d = ((z - u) / 10);
-					a = d % 10;
-					b = ((d - u) / 10);
-					if (b == 0)
-						b++;
-
 					_putchar(44);
 					_putchar(32);
-					_putchar(b + '0');
-					_putchar(a + '0');
-					_putchar(u + '0');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else if (z > 9)
 				{
-					u = z % 10;
-					d = ((z - u) / 10);
-
 					_putchar(44);
 					_putchar(32);
 					_putchar(32);
-					_putchar(d + '0');
-					_putchar(u + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else
 				{
@@ -60,5 +48,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
