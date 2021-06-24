53f04228be6711eabbb26afdb51cc12ddfa22b4e
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-06-24 07:26:42 -0700

    prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_square - draws a diagonal line on the terminal
+ * @size :- integer value
+ * Return: Nothing
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
