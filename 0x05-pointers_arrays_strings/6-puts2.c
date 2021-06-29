751f45c168e4caed5335e8920cae77d9bbab66a2
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-06-29 10:11:57 -0700

    prints every other character of a string, starting with the first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
+
 /**
- * puts2 - prints every other character of a string
- * @str: string
+ * puts2 - prints every other character of a  string to stdout
+ * @str: string to print
  *
  * Return: void
  */
