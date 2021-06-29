55d965828e8769fecd3add4422096d7392e95413
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-06-29 11:28:44 -0700

    prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,29 +10,20 @@ void puts_half(char *str)
 {
 	int i = 0, n;
 
-	while (*(str + i) != 0)
+	while (*(str + i) != '\0')
 		i++;
 
 	n = i;
 
 	if (n % 2 == 0)
-	{
 		n = n / 2;
-
-		while (*(str + n) != '\0')
-		{
-			_putchar(*(str + n));
-			n++;
-		}
-	}
 	else
+		n = (n + 1) / 2;
+
+	while (n < i)
 	{
-		n = (n - 1) / 2;
-		while (*(str + n) != '\0')
-		{
-			_putchar(*(str + n));
-			n++;
-		}
+		_putchar(*(str + n));
+		n++;
 	}
 	_putchar('\n');
 }
