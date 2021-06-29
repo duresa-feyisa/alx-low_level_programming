0976e6557e3691eb4f41622a143a8dd9d8233fd8
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-06-29 10:18:48 -0700

    prints every other character of a string, starting with the first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (*(str + i) != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
 	}
 	_putchar('\n');
 }
