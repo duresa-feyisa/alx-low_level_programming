16e2b18e5dc398c64d594f14717cc8248b79c30d
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-07-01 01:15:19 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strncat - concatenates two strings up to n *src string
+ * @dest: string where we want to append
+ * @src: string to be appended
+ * @n: maximum number of characters to be appended
+ * Return: the pointer to the string(dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i))
+		i++;
+
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	*(dest + i)='\0';
+	return (dest);
+}
