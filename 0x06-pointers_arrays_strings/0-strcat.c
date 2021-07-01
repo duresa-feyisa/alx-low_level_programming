c37799ac485412481caf8253975ba9e0b4303d7e
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-06-30 23:18:03 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _stract - concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * Return: destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i))
+		i++;
+
+	while (*(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
