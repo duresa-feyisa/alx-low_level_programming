4d969c434890a5df7707959137d493f43aa32511
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-07-01 07:02:41 -0700

    changes all lowercase letters of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string pointer
+ * Return: the pointer to the string(str)
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if(str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
