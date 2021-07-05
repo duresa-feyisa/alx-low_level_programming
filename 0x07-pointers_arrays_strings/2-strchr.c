b4f5d0749fad6e5a6b92428782b1e46583dcf0ca
duresa-feyisa <duresafeyisa2021@gmail.com>
2021-07-05 09:43:03 -0700

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s:The string in which the character is searched
+ * @c:The character that is searched in the string str
+ * Return: The pointer to the first occurrence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return (0);
+}
